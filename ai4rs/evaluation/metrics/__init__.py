c2c4affe439ac0eb1d170fc88c6ed878363e43f2
wokaikaixinxin <961211925@qq.com>
2025-08-04 23:02:41 +0800

    Revert "update gsdet readme"
    This reverts commit 9085692f7091ddda7b2ff1b310196adc0e104591.

--- ai4rs/evaluation/metrics/__init__.py
+++ ai4rs/evaluation/metrics/__init__.py
@@ -3,7 +3,6 @@ from .dota_metric import DOTAMetric
 from .rotated_coco_metric import RotatedCocoMetric
 from .icdar2015_metric import ICDAR2015Metric
 from .coco_metric_sardet_100k import CocoMetricSARDet100k
-from .fair_metric import FAIRMetric
 
 __all__ = ['DOTAMetric', 'RotatedCocoMetric', 'ICDAR2015Metric',
-           'CocoMetricSARDet100k', 'FAIRMetric']
+           'CocoMetricSARDet100k']
