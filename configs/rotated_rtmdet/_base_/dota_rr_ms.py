e548914283e3a11e7631f334a68fbdbcdc53b656
wokaikaixinxin <dingzeyu@cumt.edu.cn>
2025-09-03 22:33:42 +0800

    update rotated rtmdet dota_rr.py and dota_rr_ms.py

--- configs/rotated_rtmdet/_base_/dota_rr_ms.py
+++ configs/rotated_rtmdet/_base_/dota_rr_ms.py
@@ -62,8 +62,8 @@ train_dataloader = dict(
         filter_cfg=dict(filter_empty_gt=True),
         pipeline=train_pipeline))
 val_dataloader = dict(
-    batch_size=1,
-    num_workers=2,
+    batch_size=4,
+    num_workers=4,
     persistent_workers=True,
     drop_last=False,
     sampler=dict(type='DefaultSampler', shuffle=False),
@@ -82,8 +82,8 @@ val_evaluator = dict(type='DOTAMetric', metric='mAP')
 # inference on test dataset and format the output results
 # for submission. Note: the test set has no annotation.
 test_dataloader = dict(
-    batch_size=8,
-    num_workers=8,
+    batch_size=4,
+    num_workers=4,
     persistent_workers=False,
     drop_last=False,
     sampler=dict(type='DefaultSampler', shuffle=False),
