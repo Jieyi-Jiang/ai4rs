40810f2c18ad7ca9dc08e2127c363b76d0d52277
wokaikaixinxin <dingzeyu@cumt.edu.cn>
2025-09-28 21:34:44 +0800

    update data_preprocessors

--- ai4rs/models/__init__.py
+++ ai4rs/models/__init__.py
@@ -1,5 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .backbones import *  # noqa: F401, F403
+from .data_preprocessors import *  # noqa: F401, F403
 from .dense_heads import *  # noqa: F401, F403
 from .detectors import *  # noqa: F401, F403
 from .layers import *  # noqa: F401, F403
