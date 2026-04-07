fcba91baf6c66e8154425c3f187475f00eba6020
Jieyi Jiang <jieyi_jiang@qq.com>
2026-04-07 09:10:52 +0800

    添加 rotated_dino 和 rotated_rtdetr

--- projects/rotated_rtdetr/configs/rotated_rtdetr_r50vd_1x16_72e_dior.py
+++
@@ -1,9 +0,0 @@
-_base_ = [
-    '../../../configs/_base_/datasets/dior.py',
-    '../../../configs/_base_/schedules/schedule_1x.py',
-    '../../../configs/_base_/default_runtime.py'
-]
-
-model = dict(
-    Type=''
-)
